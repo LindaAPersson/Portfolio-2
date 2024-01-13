7b0980f5511ad3666b19644fb6bbc5cc7ac4f359
Linda Persson <lindapersson-@live.se>
2024-01-13 13:16:21 +0000

    <docs>[question.js]created questions.js and added five questions/answers

---
+++ assets/js/questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What color are bananas?"
+        answer: "A. Yellow",
+        options: [
+            'A. Yellow'
+            'B. Blue'
+            'C. Green'
+            'D. Pink'
+        ]
+    },
+    {
+        numb: 2,
+        question: "What color are tomatoes?"
+        answer: "B. Red",
+        options: [
+            'A. Black'
+            'B. Red'
+            'C. Grey'
+            'D. Brown'
+        ]
+    },
+    {
+        numb: 3,
+        question: "What color is milk?"
+        answer: "D. White",
+        options: [
+            'A. Green'
+            'B. Brown'
+            'C. Orange'
+            'D. White'
+        ]
+    },
+    {
+        numb: 1,
+        question: "What color is snow?"
+        answer: "C. White",
+        options: [
+            'A. Red'
+            'B. Blue'
+            'C. White'
+            'D. Purple'
+        ]
+    },
+    {
+        numb: 1,
+        question: "What color are oranges?"
+        answer: "B. Orange",
+        options: [
+            'A. Yellow'
+            'B. Orange'
+            'C. Black'
+            'D. Pink'
+        ]
+    },
+
+
+
+
+]
