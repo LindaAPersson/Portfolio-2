ca72c54b7ff7ff5a69c496e3f7abbfb3398ee48b
Linda Persson <lindapersson-@live.se>
2024-01-18 12:22:58 +0000

    <feat>[questions,js]added more questions, and placed them in questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,10 @@
+// questions
 let questions = [{
     question: "What color are bananas?",
     answer: [
         { text: 'Yellow', correct: true },
         { text: 'Blue', correct: false },
-        { text: 'Green', correct: false },
+        { text: 'Purple', correct: false },
         { text: 'Pink', correct: false }
     ]
 },
@@ -12,10 +13,9 @@ let questions = [{
     answer: [
         { text: 'Grey', correct: false },
         { text: 'Black', correct: false },
-        { text: 'Brown', correct: false },
+        { text: 'Pink', correct: false },
         { text: 'Red', correct: true }
     ]
-
 },
 {
     question: "What color is milk?",
@@ -38,10 +38,55 @@ let questions = [{
 {
     question: "What color are oranges?",
     answer: [
-        { text: 'Yellow', correct: false },
+        { text: 'Grey', correct: false },
         { text: 'Orange', correct: true },
         { text: 'Black', correct: false },
         { text: 'Pink', correct: false }
+    ]
+},
+{
+    question: "What color is a firetruck?",
+    answer: [
+        { text: 'Blue', correct: false },
+        { text: 'Green', correct: false },
+        { text: 'Yellow', correct: false },
+        { text: 'Red', correct: true }
+    ]
+},
+{
+    question: "What color are carrots?",
+    answer: [
+        { text: 'Pink', correct: false },
+        { text: 'Purple', correct: false },
+        { text: 'Orange', correct: true },
+        { text: 'Blue', correct: false }
+    ]
+},
+{
+    question: "What color is the sun?",
+    answer: [
+        { text: 'Pink', correct: false },
+        { text: 'Yellow', correct: true },
+        { text: 'Green', correct: false },
+        { text: 'Blue', correct: false }
+    ]
+},
+{
+    question: "What color are the clouds?",
+    answer: [
+        { text: 'White', correct: true },
+        { text: 'Brown', correct: false },
+        { text: 'Red', correct: false },
+        { text: 'Green', correct: false }
+    ]
+},
+{
+    question: "What color is broccoli?",
+    answer: [
+        { text: 'Pink', correct: false },
+        { text: 'Black', correct: false },
+        { text: 'Red', correct: false },
+        { text: 'Green', correct: true }
     ]
 }
 ];
